6534a81a88e336271c68085a573d722d413ea228
Josh Hill <josh.hill@cloudsolutions.co.uk>
2020-01-23 14:42:28 +0000

    Updates bootstrapper.sh
    Uses cloud build to build the image to remove the dependency on Docker.

--- src/bootstrapper.sh
+++ src/bootstrapper.sh
@@ -7,5 +7,5 @@ else
 fi
 
 { gsutil ls ${URL} >/dev/null && echo "Bucket already exists"; } || \
-{ gcloud auth configure-docker -q && docker build -t $IMAGE_NAME $DOCKERFILE_PATH && docker push $IMAGE_NAME && \
-docker rmi $IMAGE_NAME && gcloud container images delete $IMAGE_NAME --quiet --force-delete-tags; }
+{ gcloud --project $PROJECT builds submit --tag $IMAGE_NAME .;
+gcloud container images delete $IMAGE_NAME --quiet --force-delete-tags; }
