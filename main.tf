10cc133b01f6844346bbe89e1f199e9bd457a003
Josh Hill <josh.hill@cloudsolutions.co.uk>
2020-02-24 09:10:52 +0000

    Revert "Revert "Update module""
    This reverts commit 44e515d9241c9a7c358c4d179e17e8c3168704a0.

--- main.tf
+++ main.tf
@@ -14,54 +14,21 @@
  * limitations under the License.
  */
 
-data "google_container_registry_repository" "default" {
-  project = var.project
-  region  = var.image_region
-}
-
-data "google_container_registry_image" "bootstrapper" {
-  project = var.project
-  name    = "bootstrapper"
-  region  = var.image_region
-}
-
-resource "null_resource" "bootstrap_image_build" {
-  triggers = {
-    time = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "${path.module}/src/bootstrapper.sh"
-
-    environment = {
-      REGION          = var.image_region
-      PROJECT         = var.project
-      IMAGE_NAME      = data.google_container_registry_image.bootstrapper.image_url
-      DOCKERFILE_PATH = "${path.module}/src"
-    }
-  }
-}
-
-locals {
-  gcr_bucket = var.image_region == "" ? "artifacts.${var.project}.appspot.com" : "${var.image_region}.artifacts.${var.project}.appspot.com"
+resource "google_container_registry" "default" {
+  project  = var.project
+  location = var.image_region
 }
 
 resource "google_storage_default_object_access_control" "public_rule" {
   count  = var.public ? 1 : 0
-  bucket = local.gcr_bucket
+  bucket = google_container_registry.default.id
   role   = "READER"
   entity = "allUsers"
-
-  depends_on = [
-    null_resource.bootstrap_image_build
-  ]
 }
 
 resource "google_storage_bucket_acl" "image-store-acl" {
   count  = var.public ? 1 : 0
-  bucket = local.gcr_bucket
+  bucket = google_container_registry.default.id
 
   predefined_acl = "publicRead"
-  depends_on = [
-    null_resource.bootstrap_image_build
-  ]
 }
